538408d42f442c8dd6ba893d243b9dfcc9e9edb4
noob-mahfuj <gitmahfuj@gmail.com>
2025-05-05 13:49:38 +0600

    changed

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const mainContent = document.getElementById("content");
     const contactBtn = document.getElementById("contact-btn");
     const dropdownList = document.getElementById("dropdownlst");
-    const theme = document.querySelector(".theme")
     const bgChange = document.getElementById("bg-change");
     const btn = document.querySelectorAll(".btn");
     const projectBtn = document.querySelectorAll(".projects");
     const email = document.getElementById("email")
     const footer = document.querySelector(".footer");
     const img = document.querySelectorAll(".img");
+    const img2 = document.querySelectorAll(".mahfuj")
 
 
 
     changeBtn.addEventListener("click", () => {
         dropdownList.classList.toggle("hidden");
-        dropdownList.classList.add("flex", "flex-col", "justify-center", "gap-6", "font-semibold");
+        dropdownList.classList.add("flex", "flex-col", "justify-center", "gap-6",);
 
 
     });
@@ -24,14 +24,18 @@ document.addEventListener("DOMContentLoaded", () => {
         mainContent.classList.toggle("bg-[#121212]");
         mainContent.classList.toggle("text-white");
         contactBtn.classList.toggle("bg-white");
-        contactBtn.classList.toggle("text-[#121212]");
+        contactBtn.classList.toggle("text-black");
         btn.forEach((btns) => {
             btns.classList.toggle("bg-white");
-            btns.classList.toggle("text-[#121212]")
+            btns.classList.toggle("text-black");
             btns.classList.toggle("hover:text-white")
             btns.classList.toggle("hover:bg-[#121212]");
             btns.classList.toggle("hover:border-white");
         });
+
+        img2.forEach((imgs2) => {
+         imgs2.classList.toggle("bg-[#121212]")
+        })
 
         img.forEach((imgs) => {
             imgs.classList.toggle("invert")
